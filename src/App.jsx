8ae79b2889822562919f7204375d3a7cd03659a1
Alberto Cid <alberto.e.cid@gmail.com>
2025-02-16 02:28:43 -0300

    v22 - Hito 3 Ready - New Cart.jsx and improve CardPizza.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 // App.jsx
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Navbar';
-import Home from './Home';
-import Footer from './Footer';
-
-import RegisterPage from './RegisterPage';
-import LoginPage from './LoginPage';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div id="root">
+    <div>
       <Navbar />
-      <main>
-        {/* <Home /> */}
-        <RegisterPage />
-        {/* <LoginPage /> */}
-      </main>
+      {/* <RegisterPage /> */}
+      {/* <LoginPage /> */}
+      {/* <Cart /> */}
+      <Home />
       <Footer />
     </div>
   );
