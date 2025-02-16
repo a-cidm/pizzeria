59524cc1d3bea94ab7c1f450de5f84f3c3c20449
Alberto Cid <alberto.e.cid@gmail.com>
2025-02-16 02:32:46 -0300

    v25 - Hito 3 Ready - Improve pizza details and list ingredients from data

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,13 +1,19 @@
 import PropTypes from "prop-types";
 
+const capitalize = (text) => text.charAt(0).toUpperCase() + text.slice(1);
+
 const CardPizza = ({ name, price, ingredients, img }) => (
   <div className="card">
     <img src={img} className="card-img-top" alt={name} />
     <div className="card-body">
-      <h5 className="card-title">{name}</h5>
+      <h5 className="card-title">{capitalize(name)}</h5>
       <p className="ingredients-title">Ingredientes:</p>
-      <p className="ingredients-list">🍕 {ingredients.join(", ")}</p>
-      <p className="price ">Precio: ${price.toLocaleString()}</p>
+      <ul className="ingredients-list">
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{capitalize(ingredient)}</li>
+        ))}
+      </ul>
+      <p className="price">Precio: ${price.toLocaleString()}</p>
       <div className="buttons">
         <button className="btn btn-outline-dark">Ver Más 👀</button>
         <button className="btn btn-dark ms-2">Añadir 🛒</button>
